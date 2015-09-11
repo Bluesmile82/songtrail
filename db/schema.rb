861f40337e49bbb534f0baf2cfd1c78997c1bc31
Alvaro <pursuance@gmail.com>
2015-09-11 04:24:46 +0200

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821182328) do
+ActiveRecord::Schema.define(version: 20150910183258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "songs", force: :cascade do |t|
-    t.string   "title"
-    t.string   "author"
-    t.string   "times"
-    t.string   "key"
-    t.string   "tempo"
-    t.string   "tab"
-    t.string   "backing_track"
-    t.string   "completeness"
-    t.string   "style"
-    t.text     "lyrics"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+  create_table "answers", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "day_id"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
+  add_index "answers", ["day_id"], name: "index_answers_on_day_id", using: :btree
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
+
+  create_table "days", force: :cascade do |t|
+    t.text     "feeling"
+    t.string   "song"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text     "desire"
+    t.text     "pain"
+    t.integer  "pain_level"
+    t.string   "dreams"
+    t.string   "poem"
+    t.string   "new"
+    t.string   "artist"
+    t.string   "journal"
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.string   "city"
+    t.date     "last_date_seen"
+    t.string   "mood"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.boolean  "personal"
+    t.string   "text"
+    t.integer  "day_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "questions", ["day_id"], name: "index_questions_on_day_id", using: :btree
+
+  add_foreign_key "answers", "days"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "days"
 end
