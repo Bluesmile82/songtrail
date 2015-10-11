b92fc0a7126b7fc79c5758f42db511d476d9a02f
Alvaro <pursuance@gmail.com>
2015-10-11 20:48:31 +0200

    add some gems and clean navbar

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  url        :string
+#  author     :string
+#  title      :string
+#  day_id     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Song < ActiveRecord::Base
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  url        :string
+#  author     :string
+#  title      :string
+#  day_id     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Song, :type => :model do
