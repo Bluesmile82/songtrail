cff33152bf3680010187a814a9701e5fc57c73ce
Alvaro Leal <aleal@icometrix.com>
2017-07-24 01:03:59 +0200

    Order songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,28 +1,19 @@
 class SongsController < ApplicationController
   before_action :set_song, only: [:show, :edit, :update, :destroy]
-
-  # GET /songs
-  # GET /songs.json
   def index
-    @songs = Song.all
+    @songs = Song.all.sort
   end
 
-  # GET /songs/1
-  # GET /songs/1.json
   def show
   end
 
-  # GET /songs/new
   def new
     @song = Song.new
   end
 
-  # GET /songs/1/edit
   def edit
   end
 
-  # POST /songs
-  # POST /songs.json
   def create
     @song = Song.new(song_params)
 
@@ -37,8 +28,6 @@ class SongsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /songs/1
-  # PATCH/PUT /songs/1.json
   def update
     respond_to do |format|
       if @song.update(song_params)
@@ -51,8 +40,6 @@ class SongsController < ApplicationController
     end
   end
 
-  # DELETE /songs/1
-  # DELETE /songs/1.json
   def destroy
     @song.destroy
     respond_to do |format|
